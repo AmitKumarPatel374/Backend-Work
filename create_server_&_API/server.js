9a3bf0b2ba3475407689b9da28fc4bd15326a7a0
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-09-25 19:52:08 +0530

    add folder create_server_&_API

---
+++ create_server_&_API/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Array to store data
+let users = [];
+
+// GET route -> check array data
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// POST route -> add new user in array
+app.post("/users", (req, res) => {
+  const newUser = req.body;  // jo Postman se bheja gaya
+  users.push(newUser);
+  res.status(201).json({
+    message: "User added successfully!",
+    data: newUser,
+  });
+});
+
+
+app.listen(3000,()=>{
+    console.log("runnig on port 3000");
+    
+})
