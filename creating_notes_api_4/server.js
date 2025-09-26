528ff99e9a404ae095efe46fe749e35e0c41fdbe
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-09-26 19:57:46 +0530

    changes folders name

--- creating_notes_api_4/server.js
+++ creating_notes_api_4/server.js
@@ -1,35 +1,32 @@
 const express = require("express");
-
 const app = express();
-
 app.use(express.json());
 
 const notes = [];
 
-app.get('/notes',(req,res)=>{
+app.get('/notes', (req, res) => {
     res.send(notes);
 })
 
-app.post("/notes",(req,res)=>{
+app.post("/notes", (req, res) => {
     notes.push(req.body);
     res.send("note added successfully");
 })
 
-app.delete("/notes/:index",(req,res)=>{
+app.delete("/notes/:index", (req, res) => {
     const index = req.params.index
     delete notes[index]
     res.send("note deleted succesfully!");
 })
 
-app.patch("/notes/:index",(req,res)=>{
+app.patch("/notes/:index", (req, res) => {
     const index = req.params.index;
     const description = req.body.description;
 
     notes[index].description = description;
-
     res.send("notes updated successfully!")
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("runnig on port 3000");
 })
