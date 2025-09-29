19e1b136c710bb6dd9ab8d3a446d27521c5c6222
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-09-29 13:18:15 +0530

    new

--- create-user/server.js
+++ create-user/server.js
@@ -5,38 +5,38 @@ const mongoose = require("mongoose")
 const userModel = require("./src/model/user.model")
 
 mongoose.connect("")
-.then((res)=>console.log("mongoDb connected"))
-.catch((err)=>{
-    console.log("error->",err);
-})
-        
-app.post("/create-user",async(req,res)=>{
+    .then((res) => console.log("mongoDb connected"))
+    .catch((err) => {
+        console.log("error->", err);
+    })
+
+app.post("/create-user", async (req, res) => {
     try {
         let userObj = {
-            name:"amit",
-            age:20,
-            email:"amit@123",
-            password:"123456"
+            name: "amit",
+            age: 20,
+            email: "amit@123",
+            password: "123456"
 
         }
 
         let newUser = await userModel.create({
-            name:userObj.name,
-            age:userObj.age,
-            email:userObj.email,
-            password:userObj.password
+            name: userObj.name,
+            age: userObj.age,
+            email: userObj.email,
+            password: userObj.password
         })
         console.log(newUser);
 
-        res.send({message:"user created",user:newUser});
-        
+        res.send({ message: "user created", user: newUser });
+
 
     } catch (error) {
-        console.log("error->",error);
+        console.log("error->", error);
     }
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("running on port 3000");
 })
--- create-user/src/model/user.model.js
+++ create-user/src/model/user.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
     name: String,
-    age : Number,
-    email : String,
-    password : String
+    age: Number,
+    email: String,
+    password: String
 })
 
-const userModel = mongoose.model("user",userSchema);
+const userModel = mongoose.model("user", userSchema);
 module.exports = userModel;
